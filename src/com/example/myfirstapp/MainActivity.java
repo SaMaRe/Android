60db35563601ffb21ffcb2a008b9e6fd927db324
SaMaRe <amaet_12@hotmail.com->
2014-02-10 18:54:54 -0500

    Added all

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.myfirstapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +16,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		EditText et =(EditText) findViewById(R.id.input_text);
+		final TextView tv = (TextView) findViewById(R.id.output_text);
+		et.addTextChangedListener(new TextWatcher(){
+						@Override
+						public void afterTextChanged(Editable s){
+								tv.setText(s);
+						}
+						@Override
+						public void beforeTextChanged(CharSequence s, int start, int count, int after){
+								tv.setText(s);
+							
+						}
+						@Override
+						public void onTextChanged(CharSequence s, int start, int before, int count){
+								tv.setText(s);
+						}});
+	
+		
 	}
+	public void showToast(View view){
+		Toast.makeText(this, "My first Tast!", Toast.LENGTH_SHORT).show();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
 	}
 
 }
