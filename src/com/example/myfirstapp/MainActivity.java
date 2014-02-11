ec582ec59b8859c9bf121d84dc72db0c72f87589
SaMaRe <amaet_12@hotmail.com->
2014-02-11 15:51:02 -0500

    hw2Code

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -2,43 +2,35 @@ package com.example.myfirstapp;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.Menu;
 import android.view.View;
+
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		EditText et =(EditText) findViewById(R.id.input_text);
-		final TextView tv = (TextView) findViewById(R.id.output_text);
-		et.addTextChangedListener(new TextWatcher(){
-						@Override
-						public void afterTextChanged(Editable s){
-								tv.setText(s);
-						}
-						@Override
-						public void beforeTextChanged(CharSequence s, int start, int count, int after){
-								tv.setText(s);
-							
-						}
-						@Override
-						public void onTextChanged(CharSequence s, int start, int before, int count){
-								tv.setText(s);
-						}});
-	
 		
+		
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main);
 	}
-	public void showToast(View view){
-		Toast.makeText(this, "My first Tast!", Toast.LENGTH_SHORT).show();
-
+	private EditText et;
+	private TextView tv; 
 	
+
+	public void buttonOnClick(View v){
+				
+			et =(EditText) findViewById(R.id.input_text);
+			tv = (TextView) findViewById(R.id.output_text);
+			tv.setText(et.getText());
+			
 	}
+		
+
 
 }
