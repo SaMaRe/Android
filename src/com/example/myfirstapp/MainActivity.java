7635005f98a58961a3e30eb1fa00f82b71382feb
SaMaRe <amaet_12@hotmail.com->
2014-02-24 14:23:26 -0500

    Settings_Preference

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myfirstapp;
 
 import android.os.Bundle;
+
 import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.content.Intent;
@@ -18,16 +19,53 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+	private TextView tv;
+	private EditText et;
   
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		et =(EditText) findViewById(R.id.input_text);
+		tv =(TextView) findViewById(R.id.output_text);
+		et.addTextChangedListener(new TextWatcher(){
+						@Override
+						public void afterTextChanged(Editable s){
+							
+						}
+						@Override
+						public void beforeTextChanged(CharSequence s, int start, int count, int after){
+							
+						}
+						@Override
+						public void onTextChanged(CharSequence s, int start, int before, int count){
+							
+							
+						}});
+			
+		}
+	public void showToast(View view){
+		Toast.makeText(this, "My first Toast!", Toast.LENGTH_SHORT).show();
+		
 	}
-	public void toNewActivity(View view){
-		Intent intent = new Intent(this, NewActivity.class);
-		startActivity(intent);
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu){
+			getMenuInflater().inflate(R.menu.main,  menu);
+			return true;
+	
 	}
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item){
+				Toast.makeText(this, "menu option selceted", Toast.LENGTH_SHORT).show();
+				if(item.getItemId() == R.id.clear_text){
+					et.setText("");
+					tv.setText("");
+				}else if(item.getItemId() == R.id.action_settings){
+					Intent intent = new Intent(this, SettingsActivity.class);
+					startActivity(intent);
+				}
+				return super.onOptionsItemSelected(item);
+		}
 		
 
 
---
+++ src/com/example/myfirstapp/NewActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NewActivity extends Activity{
+	
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.new_activity);
+		}
+
+
+}
